3c2e0a31a358a1fd95dc8b561051fae9879e02f4
Jonas <35976079+JonasAlpha@users.noreply.github.com>
2020-03-21 18:05:05 +0100

    ✏️ New Features and Code Documentation
    Added:
         - Custom URL Support
         - A way to Delete URLs
    
    Removed:
         - Some minor changes
    
    Changed:
         - ReadME
         - Website Layout
         - Footer now shows Version from package.json

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ThatsAShort.link
-The magic thats running ThatsAShort.link | Very much inspired by WebDevSimplified
+A simpler URL Shortener that actually works. 
 
 ## Installation
 ```
@@ -18,8 +18,9 @@ By Default this App will use the Port 5000. You can change it in the config.json
 Make sure you have MongoDB installed on your System! This is currently crucial for making it work.
 
 ## Comming soon
-- [x] Custom short URLs (done but not merged yet)
-- [ ] Easy way to Delete short URLs
+- [x] Custom short URLs
+- [x] Easy way to Delete short URLs
+- [ ] Login (Probably Login with Google)
 
 ## FAQ
 
@@ -28,7 +29,7 @@ Help! It's not starting. I get a `MongooseServerSelectonError: connect ECONNREFU
 
 
 ## Special Thanks to
-Web Dev Simplified for making a Tutorial on how to make a URL Shortener. This was my first Time ever making something like this. Check out [his Video here](https://www.youtube.com/watch?v=SLpUKAGnm-g)
+Web Dev Simplified for making a Tutorial on how to make a URL Shortener. This was my first Time ever making something like this. Check out [his Video here](https://www.youtube.com/watch?v=SLpUKAGnm-g).
 
 ## License
 [Apache](https://github.com/JNSAPH/ThatsAShort.link/blob/master/LICENSE)
--- server.js
+++ server.js
@@ -2,26 +2,51 @@ const express = require('express')
 const mongoose = require('mongoose')
 const ShortUrl = require('./models/shortUrl')
 const config = require('./config.json')
+const package = require('./package.json')
 const app = express()
 
 mongoose.connect('mongodb://localhost/urlShortener', {
   useNewUrlParser: true, useUnifiedTopology: true
 })
 
+// Set View engine and some basic express Stuff
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 
+// Express Setup
 app.get('/', async (req, res) => {
   const shortUrls = await ShortUrl.find()
-  res.render('index', { shortUrls: shortUrls })
+  res.render('index', { shortUrls: shortUrls , package: package })
 })
 
+// User types in URL
 app.post('/shorten', async (req, res) => {
-  await ShortUrl.create({ full: req.body.fullUrl })
+
+  // APH needs to rewrite this!
+  if (req.body.customShort) {
+    await ShortUrl.create({ 
+        // User priveded a Custom Alias
+        full: req.body.fullUrl, 
+        short: req.body.customShort
+      })
+  } else {
+    await ShortUrl.create({ 
+        // No Alias provided | (Custom ID gets generated in /models/shortUrl.js)
+        full: req.body.fullUrl, 
+      })
+  }
 
   res.redirect('/')
 })
 
+// User deletes URL
+app.post('/delete', async (req, res) => {
+  ShortUrl.deleteOne({ short: req.body.delete }, function (err) {});
+
+  res.redirect('/')
+})
+
+// User tries to access URL
 app.get('/:shortUrl', async (req, res) => {
   const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
   if (shortUrl == null) return res.sendStatus(404)
