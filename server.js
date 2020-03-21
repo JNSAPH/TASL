4d9a6e502e847d4ead3ae007552dedad334202f2
JNSAPH <JNSAPH@icloud.com>
2020-03-22 00:42:46 +0100

    ⭐️Added a (basic) Login System
    For now this will do until i have more time to make a proper Login System. Please change your Login Details in config.json

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const basicAuth = require('express-basic-auth')
+
 const ShortUrl = require('./models/shortUrl')
 const config = require('./config.json')
 const package = require('./package.json')
+
 const app = express()
 
 mongoose.connect('mongodb://localhost/urlShortener', {
@@ -13,6 +16,37 @@ mongoose.connect('mongodb://localhost/urlShortener', {
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 
+// User tries to access URL
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+  if (shortUrl == null) return res.sendStatus(404)
+
+  shortUrl.clicks++
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
+})
+
+// Basic ass Authentication | Will change later | Default User: admin, Default Password: 12345 (change in config.json)
+app.use((req, res, next) => {
+  const auth = {login: config.user, password: config.password}
+
+  // parse login and password from headers
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+
+  // Verify login and password are set and correct
+  if (login && password && login === auth.login && password === auth.password) {
+    // Access granted...
+    return next()
+  }
+
+  // Access denied...
+  res.set('WWW-Authenticate', 'Basic realm="401"') // change this
+  res.status(401).send('Authentication required.') // custom message
+
+})
+
 // Express Setup
 app.get('/', async (req, res) => {
   const shortUrls = await ShortUrl.find()
@@ -21,8 +55,6 @@ app.get('/', async (req, res) => {
 
 // User types in URL
 app.post('/shorten', async (req, res) => {
-
-  // APH needs to rewrite this!
   if (req.body.customShort) {
     await ShortUrl.create({ 
         // User priveded a Custom Alias
@@ -46,16 +78,7 @@ app.post('/delete', async (req, res) => {
   res.redirect('/')
 })
 
-// User tries to access URL
-app.get('/:shortUrl', async (req, res) => {
-  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
-  if (shortUrl == null) return res.sendStatus(404)
 
-  shortUrl.clicks++
-  shortUrl.save()
-
-  res.redirect(shortUrl.full)
-})
 
 console.log(`Server started on port ${config.port}`)
 
